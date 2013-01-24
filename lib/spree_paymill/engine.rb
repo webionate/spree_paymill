71c96a20783f9c1049727ce2a13464e34f28b87b
André Cedik <andre@webionate.de>
2013-01-24 11:48:38 +0100

    changed adding of payment method

--- lib/spree_paymill/engine.rb
+++ lib/spree_paymill/engine.rb
@@ -11,14 +11,16 @@ module SpreePaymill
       g.test_framework :rspec
     end
     
-    initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::PaymentMethod::Paymill
-    end
-
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+    end
+
+    initializer "spree_paymill.register.payment_methods" do |app|
+      app.config.spree.payment_methods += [
+        Spree::PaymentMethod::Paymill
+      ]
     end
 
     config.to_prepare &method(:activate).to_proc
