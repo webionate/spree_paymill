2c2ee2b6a657b939143ea9e00a156742d365a48e
André Cedik <andre@webionate.de>
2013-02-05 14:07:23 +0100

    moved handling of paymill token up in code to make sure its called removed saving of token to session

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -3,6 +3,12 @@ Spree::CheckoutController.class_eval do
     if @order.update_attributes(object_params)
 
       fire_event('spree.checkout.update')
+      # storing token in session to be able to use it when 
+      # order is being completed
+      if params[:paymillToken].present?
+        @order.payment.response_code = params[:paymillToken]
+        @order.payment.save!
+      end
       render :edit and return unless apply_coupon_code
 
       if @order.next
@@ -13,14 +19,6 @@ Spree::CheckoutController.class_eval do
         return
       end
 
-      # storing token in session to be able to use it when 
-      # order is being completed
-      if ((@order.confirmation_required? && @order.state == 'confirm') || @order.state == 'complete') && params[:paymillToken].present?
-        session[:paymillToken] = params[:paymillToken]
-        @order.payment.response_code = params[:paymillToken]
-        @order.payment.save!
-      end
-      
       if @order.state == 'complete' || @order.completed?
         flash.notice = t(:order_processed_successfully)
         flash[:commerce_tracking] = 'nothing special'
