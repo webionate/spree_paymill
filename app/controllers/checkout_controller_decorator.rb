7bf2639d2616fefeacd3890177124932101178df
Wojciech Baran <wojciech.baran@goodylabs.com>
2013-07-18 17:03:10 +0200

    Updated checkout controller to work with spree 2.0.3

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -1,27 +1,24 @@
 Spree::CheckoutController.class_eval do
   def update
     if @order.update_attributes(object_params)
-
-      fire_event('spree.checkout.update')
+        fire_event('spree.checkout.update')
+        
       # storing token in session to be able to use it when 
       # order is being completed
       if params[:paymillToken].present?
-        @order.payment.response_code = params[:paymillToken]
-        @order.payment.save!
-      end
-      render :edit and return unless apply_coupon_code
-
-      if @order.next
-        state_callback(:after)
-      else
-        flash[:error] = t(:payment_processing_failed)
-        redirect_to checkout_state_path(@order.state)
-        return
+        @order.payments.last.response_code = params[:paymillToken]
+        @order.payments.last.save!
       end
 
-      if @order.state == 'complete' || @order.completed?
-        flash.notice = t(:order_processed_successfully)
-        flash[:commerce_tracking] = 'nothing special'
+      unless @order.next
+        flash[:error] = @order.errors[:base].join("\n")
+        redirect_to checkout_state_path(@order.state) and return
+      end
+
+      if @order.completed?
+        session[:order_id] = nil
+        flash.notice = Spree.t(:order_processed_successfully)
+        flash[:commerce_tracking] = "nothing special"
         redirect_to completion_route
       else
         redirect_to checkout_state_path(@order.state)
